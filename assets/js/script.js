fae09690a249ac6a2552b60d3ecb0c9b77f1e193
AimeeJay <ajesso@post.com>
2021-03-07 23:15:27 -0500

    added prevent default

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //This adds a new task item to the list in 4 or 5 steps**
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
-  buttonEl.addEventListener("click", createTaskHandler);
+var createTaskHandler = function(event) {
+
+  event.preventDefault();
+
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+  
+};
+  formEl.addEventListener("submit", createTaskHandler);
